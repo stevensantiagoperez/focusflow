e84c5a6d5fafb813906443492c9dbe1a09564109
Steven Santiago-Perez <santiagoperezsteven@gmail.com>
2025-11-28 23:36:48 -0500

    app.ts wired corretly

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import cors from "cors";
+import taskRoutes from "./modules/tasks/task.routes";
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("FocusFlow API is live 🚀");
 });
+
+app.use("/api/tasks", taskRoutes);
 
 export default app;
