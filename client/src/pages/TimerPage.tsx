5c68c257ebd48ccd8406cd0755ab7485f04d29ab
Steven Santiago-Perez <santiagoperezsteven@gmail.com>
2026-02-20 20:26:11 -0500

    added a updateTask import

--- client/src/pages/TimerPage.tsx
+++ client/src/pages/TimerPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useMemo, useRef, useState } from "react";
-import { createSession, getTasks } from "../services/apiClient";
+import { createSession, getTasks, updateTask } from "../services/apiClient";
 
 type Task = { id: number; title: string; completed: boolean };
 
