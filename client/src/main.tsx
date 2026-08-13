7f645c4d5225ccaaef856218a55a68947aa690cf
Steven Santiago-Perez <santiagoperezsteven@gmail.com>
2026-08-13 00:56:27 -0400

    wrapped routerProvider in TimerProvider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -27,6 +27,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <TimerProvider>
+      <RouterProvider router={router} />
+    </TimerProvider>
   </React.StrictMode>
 );
